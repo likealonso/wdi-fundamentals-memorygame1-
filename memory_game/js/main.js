00d10ac9be5fc365815d893f869f6ba3ab800374
Alonso Tacanga <alonsotacanga@Alonsos-MacBook-Pro.local>
2017-09-08 12:14:46 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("you found a match!");
 	}
-	else  {
+	else {
 		alert("sorry, try again");
-	}
+	};
+
 }
-console.log("User flipped " + cardTwo);
+
+var flipCard = function(cardId) {
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);
+if (cardsInPlay.length === 2)
+	{
+	checkForMatch();
+	}
+};
+flipCard(0); 
+flipCard(2);
+
+
